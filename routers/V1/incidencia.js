6db7d6bfc30d9a8ea5aaeb34a8973dcc1d0efcdf
Adrian1706 <adrianjbuenop@gmail.com>
2023-08-25 01:21:29 -0500

    feat: :sparkles: Metodo post de incidencia.js
    Se creó el metodo post de incidencia el cual envia datos a la coleccion incidencias

--- routers/V1/incidencia.js
+++ routers/V1/incidencia.js
@@ -7,10 +7,22 @@ let db = await conexion();
 
 incidencia.get("/", async(req, res)=>{
 
-    let incidedncias =  db.collection("incidencia");
-    let result = await incidedncias.find({}).toArray();
+    let incidencias =  db.collection("incidencia");
+    let result = await incidencias.find({}).toArray();
     res.send(result);
 
+});
+
+incidencia.post("/", async(req, res) => {
+    let result;
+    let incidencias =  db.collection("incidencia");
+    try {
+        result = await incidencias.insertOne(req.body);
+        res.status(201).send(result);
+    } catch (error) {
+        console.log(error.errInfo.details.schemaRulesNotSatisfied[0]);
+        res.send();
+    }
 });
 
 export default incidencia;
