32130eecae0bbca2442fa29e4dfddc05aa1a1bdb
Adrian1706 <adrianjbuenop@gmail.com>
2023-08-25 00:50:59 -0500

    feat: :sparkles: Metodo put en la version 2
    Se creo un metodo put en la version 2 de trainer2.js

---
+++ routers/V2/trainer2.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import { conexion } from "../../db/atlas.js";
+
+let trainer2 = Router();
+
+let db = await conexion();
+
+trainer2.get("/", async(req, res)=>{
+
+    let trainers =  db.collection("trainer");
+    let result = await trainers.find({}).toArray();
+    res.send(result);
+
+});
+
+trainer2.post("/", async(req, res) => {
+    let result;
+    let trainers =  db.collection("trainer");
+    try {
+        result = await trainers.insertOne(req.body);
+        res.status(201).send(result);
+    } catch (error) {
+        console.log(error.errInfo.details.schemaRulesNotSatisfied[0]);
+        res.send();
+    }
+});
+
+
+trainer2.put("/:Id_trainer", async(req, res)=>{
+    const Id_trainer = parseInt(req.params.Id_trainer);
+    const newData = req.body; 
+
+    try {
+        const db = await con();
+        const trainers = db.collection("trainer");
+        const result = await trainers.updateOne({ Id_trainer }, { $set: newData });
+
+        if (result.matchedCount === 1) {
+            res.send("Trainer actualizado correctamente");
+        } else {
+            res.status(404).send("Trainer no encontrado");
+        }
+    } catch (error) {
+        console.error("Error al actualizar el trainer:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
+export default trainer2;
